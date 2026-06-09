1fdd8706f6dc0f4f711c76d449831cdaefb9b774
sp90 <dev@nomis.dk>
2026-06-09 15:11:17 +0200

    refactor(core,ui): decouple core table package to resolve DI circular dependency

--- libs/ng-advanced-table-ui/src/lib/components/table-surface/table-surface.ts
+++ libs/ng-advanced-table-ui/src/lib/components/table-surface/table-surface.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, contentChild, effect, inject } from '@angular/core';
+import { NatTable } from 'ng-advanced-table';
+import { NAT_TABLE_UI_CONTROLLER, NatTableUiService } from '../../shared/table-ui.service';
 
 @Component({
   selector: 'nat-table-surface',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: '<div class="surface"><ng-content /></div>',
+  template: `<div class="surface">
+    <ng-content name="table-pager" />
+
+    <ng-content />
+  </div>`,
   styleUrl: './table-surface.css',
+  providers: [NatTableUiService],
 })
-export class NatTableSurface {}
+export class NatTableSurface {
+  private readonly uiService = inject(NatTableUiService);
+  private readonly tableControllerQuery = contentChild(NAT_TABLE_UI_CONTROLLER);
+  private readonly coreTableQuery = contentChild(NatTable);
+
+  private readonly _controllerEffect = effect(() => {
+    const controller = this.tableControllerQuery() ?? this.coreTableQuery();
+
+    if (controller) {
+      this.uiService.setController(controller);
+    }
+  });
+}
