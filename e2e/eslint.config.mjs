5c1b43a8ccfbe32ee00c908111712ba6f3e14dfc
F0rty-Tw0 <artiomtofancv@gmail.com>
2026-06-23 16:43:33 +0200

    chore(e2e)!: 🔧 remove outdated comments from ESLint config

--- e2e/eslint.config.mjs
+++ e2e/eslint.config.mjs
@@ -1,6 +1,3 @@
 import baseConfig from '../eslint.config.base.mjs';
 
-// Playwright specs. The `playwright` config (in base) self-scopes to *.e2e.ts.
-// e2e is now an Nx project (project.json), so @nx/eslint infers a `lint` target
-// here and these specs are covered by `nx run-many -t lint`.
 export default baseConfig;
