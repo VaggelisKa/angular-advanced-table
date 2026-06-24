b7760bc959b6caeea6edf8bb2ab13f537e281f55
Vaggelis <e.karavasileiadis@gmail.com>
2026-06-24 12:12:19 +0200

    Fix showcase lint resolver for CI

--- apps/showcase/eslint.config.mjs
+++ apps/showcase/eslint.config.mjs
@@ -1,4 +1,26 @@
 import { angular, angularTemplate, rxjs } from 'lint-suite/eslint';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import baseConfig, { angularOverrides } from '../../eslint.config.base.mjs';
 
-export default [...baseConfig, ...angular, ...angularTemplate, ...rxjs, ...angularOverrides];
+const configDir = dirname(fileURLToPath(import.meta.url));
+
+export default [
+  ...baseConfig,
+  ...angular,
+  ...angularTemplate,
+  ...rxjs,
+  ...angularOverrides,
+  {
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          project: join(configDir, 'tsconfig.spec.json')
+        })
+      ]
+    }
+  }
+];
