c4ca520955e6a3bde41bb08bda6d5b9329e10327
Vaggelis <e.karavasileiadis@gmail.com>
2026-06-24 18:09:33 +0200

    Remove showcase eslint resolver override

--- apps/showcase/eslint.config.mjs
+++ apps/showcase/eslint.config.mjs
@@ -1,29 +1,5 @@
 import { angular, angularTemplate, rxjs } from 'lint-suite/eslint';
-import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import baseConfig, { angularOverrides } from '../../eslint.config.base.mjs';
 
-const configDir = dirname(fileURLToPath(import.meta.url));
-
-export default [
-  ...baseConfig,
-  ...angular,
-  ...angularTemplate,
-  ...rxjs,
-  ...angularOverrides,
-  {
-    // The showcase imports workspace packages that may not have dist output yet
-    // when Nx runs affected lint/build tasks in parallel. Resolve those imports
-    // through source paths so import-x linting is independent of package build order.
-    settings: {
-      'import-x/resolver-next': [
-        createTypeScriptImportResolver({
-          alwaysTryTypes: true,
-          project: join(configDir, 'tsconfig.spec.json')
-        })
-      ]
-    }
-  }
-];
+export default [...baseConfig, ...angular, ...angularTemplate, ...rxjs, ...angularOverrides];
