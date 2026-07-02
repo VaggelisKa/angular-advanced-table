f6126ddab8bbd19bc8fd47081abb9b2a097dad0e
F0rty-Tw0 <artiomtofancv@gmail.com>
2026-07-02 13:31:54 +0200

    refactor(index): ♻️ simplify exports and reorganize structure

--- libs/ng-advanced-table/src/index.ts
+++ libs/ng-advanced-table/src/index.ts
@@ -1,50 +1,23 @@
-export { NatTable } from './feature/table';
-
-export { NatTableA11yService } from './domain-logic/table-a11y.service';
-
-export { NatTableHeaderMeasurementService } from './domain-logic/table-header-measurement.service';
-
-export { NatTableReorderService } from './domain-logic/table-reorder.service';
-
-export { NatTableResizeService } from './domain-logic/table-resize.service';
-
-export { NatTableService } from './domain-logic/table.service';
-
-export { NatTableEmptyTemplate, NatTableErrorTemplate, NatTableLoadingTemplate } from './ui/table-state-templates';
-
-export { NAT_TABLE_BODY_STATE, NAT_TABLE_DATA_STATUS } from './common/table.type';
-
 export * from '@tanstack/angular-table';
 
-export type { NatTableRowRenderedEvent } from './common/events.type';
+export * from './ui';
 
-export type {
-  NatTableAccessibilityText,
-  NatTableBodyState,
-  NatTableCellTone,
-  NatTableColumnMoveDirection,
-  NatTableColumnMeta,
-  NatTableColumnReorderResult,
-  NatTableDataStatus,
-  NatTableEmptyTemplateContext,
-  NatTableErrorTemplateContext,
-  NatTableLoadingTemplateContext,
-  NatTableRowActivateEvent,
-  NatTableRowIdGetter,
-  NatTableSortIndicatorContext,
-  NatTableMode,
-  NatTableModeConfiguration,
-  NatTableUserState,
-  NatTableUiController,
-  NatTableUiState
-} from './common/table.type';
+export * from './common';
 
-export { NatTableHotkeyA11y } from './feature/hotkey-a11y.directive';
+export * from './domain-logic';
 
-export { NAT_TABLE_KEYBINDINGS, provideNatTableKeybindings } from './common/keybindings.provider';
+export { NatTable } from './table/table';
 
-export { createNatTableKeyboard, serializeShortcutValue } from './utils/keybindings';
+export { NatTableHotkeyA11y } from './hotkey-a11y/hotkey-a11y.directive';
 
-export type { NatTableKeybindings, NatTableKeyboard, NatTableShortcut } from './common/keybindings.type';
+export { NAT_TABLE_KEYBINDINGS } from './hotkey-a11y/common/keybindings.const';
 
-export { hasNatTableStateValueChanged } from './utils/table-state-value-equality.util';
+export { provideNatTableKeybindings } from './hotkey-a11y/keybindings.provider';
+
+export { createNatTableKeyboard, serializeShortcutValue } from './hotkey-a11y/utils/keybindings.util';
+
+export type { NatTableKeybindings, NatTableKeyboard, NatTableShortcut } from './hotkey-a11y/common/keybindings.type';
+
+export { NatTableReorderService } from './reorder/table-reorder.service';
+
+export { NatTableResizeService } from './resize/table-resize.service';
